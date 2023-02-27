7868c216e04863e5af652f6fd053842e7d0cc50d
Sankalpa Dutta <124279362+SankalpaDutta012@users.noreply.github.com>
2023-02-27 22:40:17 +0530

    Create source.c

---
+++ source.c
@@ -0,0 +1,12 @@
+//Find the first digit of factorial part
+#include<stdio.h>
+int main()
+{
+    float a , b ;
+    int p , q ;
+    printf("Enter the number :");
+    scanf("%f" , &a);
+    p = a , b = a - p , q = b * 10 ;
+    printf("The result will be %d" , q);
+    return 0 ;
+}
