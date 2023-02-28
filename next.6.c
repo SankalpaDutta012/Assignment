c812de173de92e302eaeb6c8d487025c7826b0f9
Sankalpa Dutta <124279362+SankalpaDutta012@users.noreply.github.com>
2023-02-28 21:59:47 +0530

    Update next.6.c

--- next.6.c
+++ next.6.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 int main() {
-    int num, originalNum, remainder, result = 0;
+    int num, a, r, result = 0;
     printf("Enter a three-digit integer: ");
     scanf("%d", &num);
     originalNum = num;
 
-    while (originalNum != 0) {
-        remainder = originalNum % 10;
-         result += remainder * remainder * remainder;
-         originalNum /= 10;
+    while (a != 0) {
+        remainder = a % 10;
+         result += r * r * r;
+         a /= 10;
     }
 
     if (result == num)
         printf("%d is an Armstrong number.", num);
     else
-        printf("%d is not an Armstrong number.", num);
+        printf("%d error. It is not an Armstrong number.", num);
 
     return 0;
 }
