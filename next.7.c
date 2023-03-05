24dcb422b9a09b4a96b053b85e0916ec8f5c9337
Sankalpa Dutta <124279362+SankalpaDutta012@users.noreply.github.com>
2023-03-05 09:58:50 +0530

    Create next.7.c

---
+++ next.7.c
@@ -0,0 +1,8 @@
+//WAP to print no. between 20 and 60
+#include<stdio.h>
+int main(){
+    int a ;
+        for (a = 0 ; a < 100 ; a++)
+        if (((a > 20) && (a < 70)) && ((a >20) && (a < 60)))
+        printf ("%d.\n" , a);
+}
