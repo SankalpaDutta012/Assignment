bf8917dbe3cd19d7a94140bc968669e09a7e2197
Sankalpa Dutta <124279362+SankalpaDutta012@users.noreply.github.com>
2023-04-03 16:52:16 +0530

    Update next.2.c

--- next.2.c
+++ next.2.c
@@ -1,9 +1,13 @@
+// Exchange and form a new number
+
 #include <stdio.h>
 int main(){
+    
     int a , b;
     printf("Enter a number:");
     scanf("%d", &a);
     b = ((a/100)*100) + (((a%100)/10)*2)*10 + (a%10);
     printf("The new number is %d.\n", b);
     return 0;
+
 }
